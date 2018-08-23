85f90e441172e9e65e8f76bd0386f2e1b6a6f605
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-08-23 17:27:37 +0300

    default code

---
+++ src/Sitecore.Support.40913/Framework/Publishing/DataPromotion/DefaultItemCloneManifestPromoter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Sitecore.Framework.Publishing.DataPromotion;
+using Sitecore.Framework.Publishing.Item;
+using Sitecore.Framework.Publishing.Locators;
+using Sitecore.Framework.Publishing.Manifest;
+
+namespace Sitecore.Support.Framework.Publishing.DataPromotion
+{
+  public class DefaultItemCloneManifestPromoter : Sitecore.Framework.Publishing.DataPromotion.DefaultItemCloneManifestPromoter
+  {
+    public DefaultItemCloneManifestPromoter(Microsoft.Extensions.Logging.ILogger<Sitecore.Framework.Publishing.DataPromotion.DefaultItemCloneManifestPromoter> logger, Sitecore.Framework.Publishing.DataPromotion.PromoterOptions options = null) : base(logger, options)
+    {
+    }
+
+    public DefaultItemCloneManifestPromoter(Microsoft.Extensions.Logging.ILogger<Sitecore.Framework.Publishing.DataPromotion.DefaultItemCloneManifestPromoter> logger, Microsoft.Extensions.Configuration.IConfiguration config) : base(logger, config)
+    {
+    }
+
+    public override async Task Promote(
+            TargetPromoteContext targetContext,
+            IManifestRepository manifestRepository,
+            IItemReadRepository sourceItemRepository,
+            IItemRelationshipRepository relationshipRepository,
+            IItemWriteRepository targetItemRepository,
+            FieldReportSpecification fieldsToReport,
+            CancellationTokenSource cancelTokenSource)
+    {
+      var options = (PromoterOptions)(typeof(Sitecore.Support.Framework.Publishing.DataPromotion.DefaultItemCloneManifestPromoter).BaseType.GetField("_options", BindingFlags.Instance | BindingFlags.NonPublic)).GetValue(this);
+
+      await base.Promote(async () =>
+      {
+        var itemWorker = CreatePromoteWorker(manifestRepository, targetItemRepository, targetContext.Manifest.ManifestId, targetContext.CalculateResults, fieldsToReport);
+
+        await ProcessManifestInBatches(
+            manifestRepository,
+            targetContext.Manifest.ManifestId,
+            ManifestStepAction.PromoteCloneVariant,
+            async (ItemVariantLocator[] batchUris) =>
+            {
+              return await DecloneVariants(targetContext, sourceItemRepository, relationshipRepository, batchUris).ConfigureAwait(false);
+            },
+            async declonedData =>
+            {
+              await Task.WhenAll(
+                          itemWorker.SaveVariants(declonedData.Select(d => d.Item1).ToArray()),
+                          relationshipRepository.Save(targetContext.TargetStore.ScDatabaseName, declonedData.ToDictionary(d => (IItemVariantIdentifier)d.Item1, d => (IReadOnlyCollection<IItemRelationship>)d.Item2)));
+            },
+            options.BatchSize,
+            cancelTokenSource);
+      },
+      cancelTokenSource);
+    }
+  }
+}
---
+++ src/Sitecore.Support.40913/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.40913")]
+[assembly: AssemblyProduct("Sitecore.Support.40913")]
+[assembly: ComVisible(false)]
